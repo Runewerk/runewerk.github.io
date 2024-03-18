b1f5b03fa4cb31eb0a1bdc2c8432f30cfa04b0bc
Gords <gordsftrl@gmail.com>
2024-03-18 22:52:02 +0900

    super hacky but works

--- docs/js/navigation.js
+++ docs/js/navigation.js
@@ -1,18 +1,17 @@
 function loadContentForPath() {
-    // Adjust the condition to better handle the root path
-    const path = window.location.pathname.split('/').pop(); // Gets the last part of the path
-    let contentFile = "welcome.html"; // Assume root path needs welcome.html
-
-    if (path === "about") {
+    const path = window.location.pathname.split('/').pop();
+    
+    if (path === "about" ) {
         contentFile = "about.html";
     } else if (path === "contact") {
         contentFile = "contact.html";
     } else if (path === "services") {
         contentFile = "services.html";
+    } else if (path === 'welcome'){
+        contentFile = 'welcome.html';
     }
-    // No need to change contentFile for root, as it defaults to "welcome.html"
-
-    htmx.ajax('GET', `docs/${contentFile}`, { target: '#content' });
+    
+    htmx.ajax('GET', contentFile, { target: '#content' });
 }
 
 document.addEventListener('DOMContentLoaded', loadContentForPath);
