03cb4c053278f0a78c9f47e90111850aceff46ed
Gords <gordsftrl@gmail.com>
2024-06-17 23:10:45 +0900

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ['./docs/**/*.{html,js}'],
   darkMode: 'selector',
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'hue-rotate': {
+          '0%': { filter: 'hue-rotate(0deg)' },
+          '100%': { filter: 'hue-rotate(360deg)' },
+        },
+      },
+      animation: {
+        'hue-rotate': 'hue-rotate 20s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
