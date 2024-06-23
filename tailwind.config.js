9845deec71608af0b9b496345fab1cf7b746de1e
Gords <gordsftrl@gmail.com>
2024-06-23 16:25:13 +0900

    improved css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,14 @@ module.exports = {
           '0%': { filter: 'hue-rotate(0deg)' },
           '100%': { filter: 'hue-rotate(360deg)' },
         },
+        'blink': {
+          '0%, 49%': { opacity: '0' },
+          '50%, 100%': { opacity: '1' },
+        },
       },
       animation: {
         'hue-rotate': 'hue-rotate 10s linear infinite',
+        'blink': 'blink 1s infinite',
       },
     },
   },
